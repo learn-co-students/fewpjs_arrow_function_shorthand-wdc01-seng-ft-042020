7813337ccc41a81c2b37ff12686434b137e281a0
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-01 12:40:29 -0400

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ function divide() {
     return 2000 / 100
 }
 
-let square = (num1) => {
-    return num1 ** 2
+let square = (num) => {
+    return num ** 2
 }
 
-let add = (num1, num2) => {
-    return num1 + num2
-}
+let add = (num1, num2) => num1 + num2
